457edc098c0a2daf27c75171493498416374c188
WWEISONG <weisong301@gmail.com>
2020-10-01 15:02:53 +1000

    boiler template backend

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask_restplus import Api
+from flask import Blueprint
+
+blueprint = Blueprint('api', __name__)
+
+api = Api(blueprint,
+          title='Film finder flask restplus api',
+          version='1.0',
+          description='film finder',
+          authorizations={
+              "TOKEN-BASED": {
+                  "type": "apiKey",
+                  "name": "API-TOKEN",
+                  "in": "header"
+              }
+          }
+          )
+
+# api.add_namespace(user_ns, path='/user')
