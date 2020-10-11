c3fdfd9999ec8a4c2b6f057a3c8849f00b9c68ec
medical-coder <aditeja8@gmail.com>
2020-10-11 17:51:57 +1100

    front end boiler code

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../style/Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HOME
+      </header>
+    </div>
+  );
+}
+
+export default Home;
