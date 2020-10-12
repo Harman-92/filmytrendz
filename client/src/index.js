52f767eb94fd9dbe990ad02c5a6332dc7470048b
medical-coder <aditeja8@gmail.com>
2020-10-12 20:12:54 +1100

    Front end boiler code update

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
 import Home from './components/Home';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
