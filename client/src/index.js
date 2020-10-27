3993c50931e102d0dec2eaa25346542e697a1fdc
medical-coder <aditeja8@gmail.com>
2020-10-28 09:38:27 +1100

    update css import

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css'
 import Header from './components/Header';
 import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 import MovieDetails from "./components/MovieDetails";
 import Home from "./components/Home";
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 const routing = (
     <Router>
